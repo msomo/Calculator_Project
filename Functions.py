66e03731a6bfec057fed6f9d9aa7966e0d8ee7a3
Mohamed Somo <msomowork@gmail.com>
2023-08-17 16:29:43 -0500

    updated the main.py

--- Functions.py
+++ Functions.py
@@ -2,29 +2,57 @@
 def addition(first, second):
     return first + second
 
-def substraction(first, second):
-    return second - first
+# def addition():
+#     first = int(input("Enter first: "))
+#     second = int(input("Enter Second: "))
+#     result = (first + second)
+#     print(result)
 
-def multiplication(first, second):
-    return first * second
 
-def division(first, second):
-    return first / second
+# def substraction():
+#     first = int(input("Enter first: "))
+#     second = int(input("Enter Second: "))
+#     return second - first
 
-def exponential(first, second):
-    return first ** second
 
-def sqrt(number):
-    return number ** 1/2
+# def multiplication(first, second):
+#     first = int(input("Enter first: "))
+#     second = int(input("Enter Second: "))
+#     return first * second
+
+# def division(first, second):
+#     first = int(input("Enter first: "))
+#     second = int(input("Enter Second: "))
+#     return first / second
+
+# def exponential(first, second):
+#     first = int(input("Enter first: "))
+#     second = int(input("Enter Second: "))
+#     return first ** second
+
+# def sqrt(number):
+#     number = int(input("Enter a number: "))
+#     return number ** 1/2
     
-def tangent():
-    height = int(input("Enter the height: "))
-    base = int(input("Enter the base: "))
-    return height / base
+# def tangent():
+#     height = int(input("Enter the height: "))
+#     base = int(input("Enter the base: "))
+#     return height / base
 
-def sin():
-    hypotenus = int(input("Enter the hypotenus: "))
-    base = int(input("Enter the base: "))
-    return hypotenus / base
+# def sin():
+#     hypotenus = int(input("Enter the hypotenus: "))
+#     base = int(input("Enter the base: "))
+#     return hypotenus / base
+
+# # def exchanges():
+# #     rate_1USD   = int(input("Enter day exchange rate: "))
+# #     amount   = int(input("Enter amount of local currecy to change: "))
+# #     currency = input("Enter the name of Currecy = ")
+# #     return currency, amount * rate_1USD
+
+
+
+
+
     
 
--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-import Functions
-# print(Functions.addition(3, 4))
-# print(Functions.sqrt(4))
-# print(Functions.tangent())
-print(Functions.sin())
+import functions
+print("my python calculator")
+choice = int(input('''
+choose one of the following choices
+[1] addition
+[2] subtraction
+[3] multiplication
+[4] division
+[5] exponential 
+[6] square root
+[7] tangent(trigonometry)
+[8] sin (trigonometry)
+[9] cosine (trigonometry)
+'''))
+
+if choice == 1:
+    functions.addition()
 
